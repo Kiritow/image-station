831847c3c1aa380d23fcde52df42598df2e44794
Kiritow <1362050620@qq.com>
2021-03-01 18:25:05 +0800

    frp: remove start script

--- frpc/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed "s/__server_addr__/$1/g; s/__server_port__/$2/g; s/__service_name__/$3/g; s/__type__/$4/g; s/__local_port__/$5/g; s/__remote_port__/$6/g" /opt/frp/frpc.ini.template > /opt/frp/frpc.ini
-/opt/frp/frpc -c /opt/frp/frpc.ini
